6b8554de72084b29119bede18d36e10ab72d228c
dev-cyan <zyos1@naver.com>
2025-06-27 12:17:29 +0900

    init: 프로젝트 초기 생성

---
+++ wavelog/src/main/java/wavelog/wavelog/WavelogApplication.java
@@ -0,0 +1,13 @@
+package wavelog.wavelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavelogApplication.class, args);
+	}
+
+}
---
+++ wavelog/src/test/java/wavelog/wavelog/WavelogApplicationTests.java
@@ -0,0 +1,13 @@
+package wavelog.wavelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
