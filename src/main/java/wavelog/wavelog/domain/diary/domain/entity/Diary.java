226968d3cde7c8583fd172b99400c46b56148d1d
dev-cyan <zyos1@naver.com>
2025-07-02 19:27:27 +0900

    feat: Diary 엔티티 연관관계 구현

--- src/main/java/wavelog/wavelog/domain/diary/domain/entity/Diary.java
+++ src/main/java/wavelog/wavelog/domain/diary/domain/entity/Diary.java
@@ -4,7 +4,13 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import wavelog.wavelog.domain.comment.Comment;
+import wavelog.wavelog.domain.hashtag.Hashtag;
+import wavelog.wavelog.domain.like.Like;
 import wavelog.wavelog.global.common.domain.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,6 +39,20 @@ public class Diary extends BaseEntity {
 
     @Column
     private String category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "diary_hashtag",
+            joinColumns = @JoinColumn(name = "diary_id"),
+            inverseJoinColumns = @JoinColumn(name = "hashtag_id")
+    )
+    private List<Hashtag> hashtags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
 
     @Builder
     public Diary(String title, String code, String content, Integer likeCount, Integer viewCount, String category) {
