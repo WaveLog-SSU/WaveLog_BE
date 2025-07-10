00031b2c9d8c77ed433b2141198d1d4832922700
dev.cyan <zyos1@naver.com>
2025-07-10 18:22:01 +0900

    chore: AI API 호출 타임아웃 시간 조정

--- src/main/java/wavelog/wavelog/global/config/OpenAiConfig.java
+++ src/main/java/wavelog/wavelog/global/config/OpenAiConfig.java
@@ -1,20 +1,22 @@
 package wavelog.wavelog.global.config;
 
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.time.Duration;
 
 
 @Configuration
 public class OpenAiConfig {
-
     @Value("${openai.api-key}")
     private String apiKey;
 
     @Bean
     public OpenAiService openAiService() {
-        return new OpenAiService(apiKey);
+        Duration timeout = Duration.ofSeconds(60);
+        return new OpenAiService(apiKey, timeout);
     }
 }
